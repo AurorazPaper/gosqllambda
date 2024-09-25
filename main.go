a724077aaa926e548f9571beeacb38468d9374f1
Aurora Jarvis <aurora.jarvis@zpaper.com>
2024-09-25 15:36:46 -0400

    first commit 9-25-24

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	_ "github.com/go-sql-driver/mysql"
@@ -38,36 +39,13 @@ type faxLog struct {
 	// did         string // zPaper: callid stripped of non-digits prefixed with leading 1 if necessary
 }
 
-// struct for columns of the inbound fax report, capitalized
-type inboundColumns struct {
-	Starttime    string
-	System       string
-	Commid       string
-	FaxFrom      string
-	FaxTo        string
-	Conntime     string
-	Npages       string
-	Entrytype    string
-	Cidname      string
-	Tsi          string
-	Reason       string
-	Receivedfile string
-}
-
-type columnTransferInfo struct {
-	OriginTable       string
-	OriginColumn      string
-	DestinationTable  string
-	DestinationColumn string
-}
-
 type GoTestEvent struct {
 	Name string `json:"name"`
 }
 
 func HandleRequestTest(ctx context.Context, event GoTestEvent) (string, error) {
 
-	// Retrieve environment variables
+	// Retrieve environment variables from Lambda config
 	rdsHost := os.Getenv("RDS_HOST")
 	rdsPort := os.Getenv("RDS_PORT")
 	dbName := os.Getenv("DB_NAME")
@@ -91,63 +69,45 @@ func HandleRequestTest(ctx context.Context, event GoTestEvent) (string, error) {
 		return "2", fmt.Errorf("error pinging database: %v", err)
 	}
 
-	faxlogs, err := queryfaxrecords("RECV")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("logs found: %v", faxlogs)
+	/*
+		faxlogs, err := queryfaxrecords("RECV")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("logs found: %v", faxlogs)
+	*/
 
+	// categorizes calls as missed or not missed
 	missedcallColumn, err := processMissedcalls(db)
 	if err != nil {
-		fmt.Println("Failure to add missedCalls column:", err)
+		fmt.Println("Failure to process missed calls:", err)
 	} else {
 		fmt.Println(missedcallColumn)
 	}
 
-	// Define the SQL statement to create a new table
-	createTableSQL := `
-		CREATE TABLE IF NOT EXISTS inboundReportTable (
-			id INT AUTO_INCREMENT PRIMARY KEY,
-			starttime DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'date from xferfaxlog MM/dd/yy HH:mm, 24 HR clock',
-			entrytype VARCHAR(6) NOT NULL DEFAULT '' COMMENT 'SEND,RECV,CALL,POLL,PAGE,UNSENT,SUBMIT,PROXY',
-			commid VARCHAR(11) NOT NULL DEFAULT '',
-			modem VARCHAR(8) DEFAULT NULL,
-			qfile VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'SEND: jobid',
-			jobtag VARCHAR(255) DEFAULT NULL COMMENT 'RECV: NULL',
-			sender VARCHAR(255) DEFAULT NULL COMMENT 'The sender/receiver electronic mailing address (facsimile receptions are always attributed to the "fax" user).',
-			localnumber VARCHAR(255) DEFAULT NULL COMMENT 'SEND: destnumber',
-			tsi VARCHAR(255) DEFAULT NULL COMMENT 'SEND: csi',
-			params VARCHAR(255) DEFAULT NULL,
-			npages VARCHAR(4) DEFAULT '0',
-			jobtime VARCHAR(255) DEFAULT NULL,
-			conntime VARCHAR(255) DEFAULT NULL,
-			reason VARCHAR(255) DEFAULT NULL,
-			cidname VARCHAR(255) DEFAULT NULL COMMENT 'SEND: faxname',
-			cidnumber VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'SEND: faxnumber',
-			callid VARCHAR(32) DEFAULT NULL COMMENT 'SEND: empty',
-			owner VARCHAR(255) DEFAULT NULL,
-			dcs VARCHAR(255) DEFAULT NULL,
-			jobinfo VARCHAR(255) DEFAULT NULL COMMENT 'totpages/ntries/ndials/totdials/maxdials/tottries/maxtries',
-			did VARCHAR(11) NOT NULL DEFAULT '' COMMENT 'zPaper: callid stripped of non-digits prefixed with leading 1 if necessary'
-		);
-		`
-
-	// Execute the SQL statement
-	_, err = db.Exec(createTableSQL)
+	// categorizes fax as complete or incomplete
+	faxprocess, err := processIncompleteFax(db)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Failure to process incomplete faxes:", err)
+	} else {
+		fmt.Println(faxprocess)
 	}
 
-	fmt.Println("Table created successfully!")
+	// gets the ID of the next successful call, writes it to a column for the original missed entry
+	successProcess, err := processNextSuccess(db)
+	if err != nil {
+		fmt.Println("Failure to process next success logic:", err)
+	} else {
+		fmt.Println(successProcess)
+	}
 
-	var inboundTransferInfo columnTransferInfo
-
-	inboundTransferInfo.OriginTable = "xferfaxlog"
-	inboundTransferInfo.OriginColumn = "datetime"
-	inboundTransferInfo.DestinationTable = "inboundReportTable"
-	inboundTransferInfo.DestinationColumn = "starttime"
-
-	// faxColumnTransfer(inboundTransferInfo)
+	// gets the ID of the next successful call, writes it to a column for the original missed entry
+	callDiff, err := missedCallDiff(db)
+	if err != nil {
+		fmt.Println("Failure to process next success logic:", err)
+	} else {
+		fmt.Println(callDiff)
+	}
 
 	return "Successfully connected to RDS", nil
 
@@ -159,98 +119,121 @@ func main() {
 
 }
 
-func queryfaxrecords(recordtype string) (interface{}, error) {
-	var faxlogs []faxLog
-
-	rows, err := db.Query("SELECT datetime, qfile, localnumber, tsi, npages, cidname, cidnumber, jobinfo FROM xferfaxlog WHERE entrytype = ? LIMIT 5", recordtype)
-	if err != nil {
-		return nil, fmt.Errorf("queryfaxrecords %q: %v", recordtype, err)
-	}
-	defer rows.Close()
-	// Loop through rows, using Scan to assign data from xferfaxlogs to struct fields
-	for rows.Next() {
-		var log faxLog
-
-		log.datetime = ""
-		log.entrytype = ""
-		log.qfile = ""
-		log.commid = ""
-		log.localnumber = ""
-		log.tsi = ""
-		log.conntime = ""
-		log.cidname = ""
-		log.cidnumber = ""
-		log.jobinfo = ""
-		log.system = ""
-
-		if err := rows.Scan(&log.datetime, &log.qfile, &log.localnumber, &log.tsi, &log.npages, &log.cidname, &log.cidnumber, &log.jobinfo); err != nil {
-			return nil, fmt.Errorf("queryfaxrecords %q: %v", recordtype, err)
-		}
-		faxlogs = append(faxlogs, log)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("queryfaxrecords %q: %v", recordtype, err)
-	}
-	return faxlogs, nil
-
-}
-
-/*
-func faxColumnTransfer(transferInfo columnTransferInfo) (string, error) {
-	// Fetch data from the source table with dynamic column name
-	query := fmt.Sprintf("SELECT %s FROM %s LIMIT 2000", transferInfo.OriginColumn, transferInfo.OriginTable)
-	rows, err := db.Query(query)
-	if err != nil {
-		return "3", fmt.Errorf("failed to query source table: %w", err)
-	}
-	defer rows.Close()
-
-	// Prepare insert statement for the target table with dynamic column name
-	insertQuery := fmt.Sprintf("INSERT INTO %s (%s) VALUES (?)", transferInfo.DestinationTable, transferInfo.DestinationColumn)
-	stmt, err := db.Prepare(insertQuery)
-	if err != nil {
-		return "4", fmt.Errorf("failed to prepare insert statement: %w", err)
-	}
-	defer stmt.Close()
-
-	// Process and insert data
-	for rows.Next() {
-		var value interface{}
-		if err := rows.Scan(&value); err != nil {
-			return "5", fmt.Errorf("failed to scan row: %w", err)
-		}
-
-		// Convert the value to a type that can be used with Exec
-		if _, err := stmt.Exec(value); err != nil {
-			return "6", fmt.Errorf("failed to insert data into target table: %w", err)
-		}
-	}
-
-	if err := rows.Err(); err != nil {
-		return "7", fmt.Errorf("error occurred during rows iteration: %w", err)
-	}
-
-	return "Data transfer successful", nil
-}
-*/
-
 // Processes Call entries, categorizes them as missed or not missed
 func processMissedcalls(db *sql.DB) (string, error) {
-	createCallMissedColumn :=
+	updateCallMissed :=
 		`UPDATE xferfaxlog
 		SET callMissed = CASE
-			WHEN entrytype = 'CALL' AND (other_column IS NOT NULL AND other_column != '') THEN TRUE
-			WHEN entrytype = 'CALL' AND (reason IS NULL OR reason = '') THEN FALSE
-			ELSE callMissed  -- Retain current value if conditions are not met
+			WHEN entrytype = 'CALL' AND (reason IS NOT NULL AND reason != '') THEN 1
+			WHEN entrytype = 'CALL' AND (reason IS NULL OR reason = '') THEN 0
+			ELSE callMissed
 		END
-		WHERE entrytype = 'CALL' AND callMissed IS NULL;  -- Limit to NULL values
+		WHERE id IN (
+			SELECT id
+			FROM (
+				SELECT id
+				FROM xferfaxlog
+				WHERE entrytype = 'CALL' AND callMissed IS NULL
+				ORDER BY datetime DESC  
+			) AS subquery
+		);
 		`
 
-	_, err := db.Exec(createCallMissedColumn)
+	_, err := db.Exec(updateCallMissed)
 	if err != nil {
-		return "", fmt.Errorf("could not create callMissed column: %w", err)
+		return "", fmt.Errorf("could not run Missed Call logic: %w", err)
 	}
 
 	return "callMissed column creation successful", nil
 
+}
+
+func processNextSuccess(db *sql.DB) (string, error) {
+	// Gets the ID of the next successful fax between two phone numbers
+	updateNextSuccess := `
+		UPDATE xferfaxlog AS missed
+		SET nextSuccess = (
+			SELECT next.id
+			FROM (
+				SELECT id, localnumber, cidname, datetime
+				FROM xferfaxlog
+				WHERE callMissed = 0
+			) AS next
+			WHERE next.localnumber = missed.localnumber
+			AND next.cidname = missed.cidname
+			AND next.datetime > missed.datetime
+			ORDER BY next.datetime ASC
+			LIMIT 1
+		)
+		WHERE missed.callMissed = 1 AND nextSuccess IS NULL
+		LIMIT 500;
+	`
+
+	startTime := time.Now()
+	if _, err := db.Exec(updateNextSuccess); err != nil {
+		return "", fmt.Errorf("could not run next success logic: %w", err)
+	}
+	duration := time.Since(startTime)
+	log.Printf("Next success logic executed in %s", duration)
+
+	return "Next success processing successful", nil
+}
+
+// Processes Call entries, categorizes them as missed or not missed
+func processIncompleteFax(db *sql.DB) (string, error) {
+	updateCallMissed :=
+		`UPDATE xferfaxlog
+		SET faxincomplete = CASE
+			WHEN entrytype = 'RECV' AND (reason IS NOT NULL AND reason != '') THEN 1
+			WHEN entrytype = 'RECV' AND (reason IS NULL OR reason = '') THEN 0
+			ELSE callMissed
+		END
+		WHERE id IN (
+			SELECT id
+			FROM (
+				SELECT id
+				FROM xferfaxlog
+				WHERE entrytype = 'RECV' AND faxincomplete IS NULL
+				ORDER BY datetime DESC  
+			) AS subquery
+		);
+		`
+
+	_, err := db.Exec(updateCallMissed)
+	if err != nil {
+		return "", fmt.Errorf("could not run incomplete fax logic: %w", err)
+	}
+
+	return "incomplete fax processing successful", nil
+
+}
+
+func missedCallDiff(db *sql.DB) (string, error) {
+	// Gets the ID of the next successful fax between two phone numbers
+	getMissDiff := `
+		UPDATE xferfaxlog AS missed
+		SET retrytime = (
+			SELECT TIMESTAMPDIFF(MINUTE, missed.datetime, next.datetime)
+			FROM (
+				SELECT id, localnumber, cidname, datetime
+				FROM xferfaxlog
+				WHERE callMissed = 0
+			) AS next
+			WHERE next.localnumber = missed.localnumber
+			AND next.cidname = missed.cidname
+			AND next.datetime > missed.datetime
+			ORDER BY next.datetime DESC
+			LIMIT 1
+		)
+		WHERE missed.callMissed = 1 AND nextSuccess IS NULL
+		LIMIT 250;
+		`
+	startTime := time.Now()
+	if _, err := db.Exec(getMissDiff); err != nil {
+		return "", fmt.Errorf("could not run next missed call difference logic: %w", err)
+	}
+	duration := time.Since(startTime)
+	log.Printf("call difference logic executed in %s", duration)
+
+	return "call difference processing successful", nil
 }
